bf587325e0a3aa313bff23ff7af6a5295f0de710
VuLeHong <144672834+VuLeHong@users.noreply.github.com>
2025-04-09 21:40:07 +0700

    .

--- thutuchanhchinh.py
+++ thutuchanhchinh.py
@@ -16,11 +16,11 @@ import math
 
 def get_page_content(url):
     options = webdriver.ChromeOptions()
-    # options.add_argument("--headless")  # Run in headless mode
+    options.add_argument("--headless")  # Run in headless mode
     options.add_argument("--disable-gpu")
     options.add_argument("--no-sandbox")
     options.add_argument("--disable-dev-shm-usage")
-    service = Service(executable_path=r"D:\Pythons\Python3.12\Lib\site-packages\selenium\webdriver\chrome\chromedriver.exe")
+    service = Service(executable_path=r"/usr/local/bin/chromedriver")
     # D:\Pythons\Python3.12\Lib\site-packages\selenium\webdriver\chrome\chromedriver.exe
     # /usr/local/bin/chromedriver
     driver = webdriver.Chrome(service=service, options=options)
